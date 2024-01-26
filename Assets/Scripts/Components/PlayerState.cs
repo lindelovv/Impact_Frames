c22a564446c6421e289fd628ef21710729e60fda
Arxhlight <102960157+Arxhlight@users.noreply.github.com>
2024-01-26 13:55:14 +0100

    Panic At the Deadline
    add

--- Assets/Scripts/Components/PlayerState.cs
+++ Assets/Scripts/Components/PlayerState.cs
@@ -1,12 +1,27 @@
+using System.Text;
 using Unity.Entities;
 using Unity.NetCode;
+using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.Collections.AllocatorManager;
+using static UnityEngine.AudioSettings;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerState : MonoBehaviour
 {
     //-----------------------
-    [Tooltip("[bool] Set if the character currently is grounded.")]
+    [Tooltip("[bool] Set if the character currently is grounded.")] // More tooltips needd?
     public bool isGrounded;
+    public bool isMobile;
+    public bool IsAnimationLocked;
+    public bool IsOnBeat;
+    public bool IsAttacked;
+    public bool IsHit;
+    public bool HasFullComboMeterEnergy;
+    public bool HasCompletedMusicalChallenge;
+    public bool IsInRange;
+    public bool IsOnCooldown;
+    public bool IsTurning;
 
     private class Baker : Baker<PlayerState>
     {
@@ -16,6 +31,16 @@ public class PlayerState : MonoBehaviour
                                                               // i världen, till skillnad från player som är Dynamic
             AddComponent(entity, new PlayerStateComponent {
                 isGrounded = authoring.isGrounded,
+                isMobile = authoring.isMobile,
+                IsAnimationLocked = authoring.IsAnimationLocked,
+                IsOnBeat = authoring.IsOnBeat,
+                IsAttacked = authoring.IsAttacked,
+                IsHit = authoring.IsHit,
+                HasFullComboMeterEnergy = authoring.HasFullComboMeterEnergy,
+                HasCompletedMusicalChallenge = authoring.HasCompletedMusicalChallenge,
+                IsInRange = authoring.IsInRange,
+                IsOnCooldown = authoring.IsOnCooldown,
+                IsTurning = authoring.IsTurning, 
             });
         }
     }
@@ -27,7 +52,59 @@ public class PlayerState : MonoBehaviour
 [GhostComponent(PrefabType = GhostPrefabType.AllPredicted)]
 public struct PlayerStateComponent : IComponentData
 {
-    // Addera alla datas för alla states, gör ett test!
+    /// <summary>
+    /// Required specifically for Jump.
+    /// </summary>
     public bool isGrounded;
+
+    /// <summary>
+    /// Required for Movement, Jump, Punch, Kick, Block, Special Move, Grab, Dash, and Air Dash.
+    /// </summary>
+    public bool isMobile;
+
+    /// <summary>
+    /// Required for Movement, Jump, Block, Grab, Dash, and Air Dash
+    /// </summary>
+    public bool IsAnimationLocked;
+
+    /// <summary>
+    /// Required Bunny Jump, Attacks 
+    /// </summary>
+    public bool IsOnBeat;
+
+    /// <summary>
+    /// Required for Perfect Block sucess, and incoming attack to calculate damage taken
+    /// </summary>
+    public bool IsAttacked;
+
+    /// <summary>
+    /// Required if Hurtbox collider is hit, to calculate damage, and Perfect Block fail
+    /// </summary>
+    public bool IsHit; 
+
+    /// <summary>
+    /// Required for pressing Special Move button.
+    /// </summary>
+    public bool HasFullComboMeterEnergy;
+
+    /// <summary>
+    /// Required for Special Move Sucess.
+    /// </summary>
+    public bool HasCompletedMusicalChallenge;
+
+    /// <summary>
+    /// Required if In Range of opponent, for Grab.
+    /// </summary>
+    public bool IsInRange;
+   
+    /// <summary>
+    /// Required for every state that can have a cooldown
+    /// </summary>
+    public bool IsOnCooldown;
+
+    /// <summary>
+    /// Required for Facing the right direction
+    /// </summary>
+    public bool IsTurning;
 }
 
--- Assets/Scripts/Systems/PlayerFightingSystem.cs
+++ Assets/Scripts/Systems/PlayerFightingSystem.cs
@@ -1,6 +1,9 @@
+using System.Diagnostics;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
+using System.Collections;
+using Unity.Physics;
 
 [BurstCompile]
 public partial struct PlayerFightingSystem : ISystem
@@ -29,8 +32,71 @@ public partial struct PlayerFightingSystem : ISystem
 public partial struct FightJob : IJobEntity
 {
     public float DeltaTime;
-    public void Execute(in InputComponentData input)  // Execute tillhör IJobEntity Interfacet
+    public void Execute(in InputComponentData input, in PlayerStateComponent psc)  // Execute tillhör IJobEntity Interfacet
     {
+
+        //Input button logik för att köra punch
+        Punch(psc);
+
+
+        //Input button logik för att köra kick
+        Kick(psc);
+    }
+
+    
+    
+    
+    public void Punch(PlayerStateComponent psc)
+    {
+        if (CanPlayerAttack(psc))
+        {
+            
+            CheckHit(psc);
+            UnityEngine.Debug.Log("Punched!");
+
+            // Set Animation Logic
+            // Set VFX Logic
+            // Set Sound Logic
+        }
+    }
+
+    public void Kick(PlayerStateComponent psc)
+    {
+        if (CanPlayerAttack(psc))
+        {
+            CheckHit(psc);
+            UnityEngine.Debug.Log("Kikecd!");
+
+            // Set Animation Logic
+            // Set VFX Logic
+            // Set Sound Logic
+        }
+    }
+
+    
+
+    private void CheckHit(PlayerStateComponent psc)
+    {
+        // Har jag träffat en fiendess hurtbox
+        //deal damage
+        // räkna hur många gånger jag träffa (3)
+        //deal more damga
+        // kalkylera på varje button klick
+       
         
     }
+
+
+
+
+    
+    private bool CanPlayerAttack(PlayerStateComponent psc)
+    {
+      
+        return psc.isGrounded && !psc.IsAnimationLocked;
+    }
+
+
+
+
 }
