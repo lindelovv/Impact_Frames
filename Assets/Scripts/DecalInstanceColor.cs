3312aa7b4f59b71c2ec22d03364155020cf42e98
Arxhlight <102960157+Arxhlight@users.noreply.github.com>
2024-01-30 14:02:25 +0100

    Decals and some extra Virtual Camera for testing + meshes

---
+++ Assets/Scripts/DecalInstanceColor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DecalInstanceColor : MonoBehaviour
+{
+    [SerializeField] Color color;
+    [SerializeField] DecalProjector projector;
+    private void Start()
+    {
+       
+        projector.material = new Material(projector.material);
+
+        projector.material.SetColor("_DecalColor", color);
+    }
+
+}
