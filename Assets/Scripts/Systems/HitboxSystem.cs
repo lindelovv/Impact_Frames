df0528f87c83ae954f5751de9283af4dc26bfae8
lindelovv <lindelowgin@gmail.com>
2024-02-18 22:28:19 +0100

    Initial system and component setup

---
+++ Assets/Scripts/Systems/HitboxSystem.cs
@@ -0,0 +1,20 @@
+﻿
+using Unity.Entities;
+
+public partial struct HitboxSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<HitboxData>();
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (
+            var hitbox
+            in SystemAPI.Query<HitboxData>()
+        ) {
+            
+        }
+    }
+}
