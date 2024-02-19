5c798cd5c6ff2ace4a8b39d4b34643bdc4a39f92
lindelovv <lindelowgin@gmail.com>
2024-02-19 18:32:51 +0100

    collisions casts working (check hit for nearby collider)

---
+++ Assets/Scripts/Systems/DestroySystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.NetCode;
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public partial struct DestroySystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<HealthComponent>();
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        var cmdBuffer = new EntityCommandBuffer(Allocator.Temp);
+        foreach (
+            var (health, entity)
+            in SystemAPI.Query<HealthComponent>()
+                .WithEntityAccess()
+        ) {
+            if (health.CurrentHealth <= 0)
+            {
+                cmdBuffer.DestroyEntity(entity);
+            }
+        }
+        cmdBuffer.Playback(state.EntityManager);
+        cmdBuffer.Dispose();
+    }
+}
