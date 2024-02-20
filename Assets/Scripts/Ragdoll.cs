66a2204c2c0a74b27a329f2ecae11d27ee896f72
lindelovv <lindelowgin@gmail.com>
2024-02-20 21:26:15 +0100

    start of ragdoll testing

---
+++ Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    public GameObject               rig;
+    public Animator            animator;
+
+    private Collider[]    limbColliders;
+    private Rigidbody[] limbRigidbodies;
+
+    public void Start()
+    {
+        limbColliders = rig.GetComponentsInChildren<Collider>();
+        limbRigidbodies = rig.GetComponentsInChildren<Rigidbody>();
+        SetRagdoll(false);
+    }
+
+    void SetRagdoll(bool shouldRagdoll)
+    {
+        if (shouldRagdoll)
+        {
+            foreach (var col in limbColliders)
+            {
+                col.enabled = true;
+            }
+            foreach (var rigid in limbRigidbodies)
+            {
+                rigid.isKinematic = false;
+            }
+            animator.enabled = false;
+        }
+        else
+        {
+            foreach (var col in limbColliders)
+            {
+                col.enabled = false;
+            }
+            foreach (var rigid in limbRigidbodies)
+            {
+                rigid.isKinematic = true;
+            }
+            animator.enabled = true;
+        }
+    }
+}
