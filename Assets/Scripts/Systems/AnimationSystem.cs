93356435a9a959e75ad1a5c53084a3e22480d286
lindelovv <lindelowgin@gmail.com>
2024-02-20 20:10:39 +0100

    Multiple improvements and new features
    Updated animation system logic
    Working impact application system
    Initial block logic implemented

--- Assets/Scripts/Systems/AnimationSystem.cs
+++ Assets/Scripts/Systems/AnimationSystem.cs
@@ -2,7 +2,6 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.NetCode;
-using Unity.Services.Relay.Models;
 using Unity.Transforms;
 using UnityEngine;
 using Random = UnityEngine.Random;
