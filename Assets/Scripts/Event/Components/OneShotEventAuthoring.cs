2f3d2c1150abdb5a2efbc9dbf84b817e8c9f896f
lindelovv <lindelowgin@gmail.com>
2024-03-12 20:52:59 +0100

    Thoughtbubble trigger system working + Script folders restructure

---
+++ Assets/Scripts/Event/Components/OneShotEventAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class OneShotEventAuthoring : MonoBehaviour
+{
+    class Baker : Baker<OneShotEventAuthoring>
+    {
+        public override void Bake(OneShotEventAuthoring authoring)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
+public struct OneShotEvent : IComponentData {}
