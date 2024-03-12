e8bd443a232f8642da746ba85b4765535fcab40b
Arxhlight <102960157+Arxhlight@users.noreply.github.com>
2024-03-12 12:55:30 +0100

    Abiance Sound Script and Functionallity
    Added a script that builds the ambiance
    Working: Yes
    Known issues: laggy at start

--- Assets/Scripts/PhilipScript/Sound_AmbianceController.cs
+++ Assets/Scripts/PhilipScript/Sound_AmbianceController.cs
@@ -3,22 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+[RequireComponent(typeof(AudioSource))]
 
+
+/**
+ * First AudioSource RequiredComponent is dedicated for the Main Ambient Clip
+ * Every other Audio Source added can be used as a new player for 1 audio clip
+ * Make sure to add more Audio Sources if More Audio Clips are used
+ * Expand Fill the Array with the new Audio Sources
+ * AudioSources[] should ALWAYS be 1 more than AudioClip[]
+ */
 public class Sound_AmbianceController : MonoBehaviour
 {
-    public AudioSource[] sources;   // Add audioSources in INSPECTOR
-   
+    public AudioSource[] sources;
 
-    public AudioClip[] sounds;
-    [Header("Audio Source Parameters: ")]
-    public float mainVol = 0.3f;
-    public float otherVol = 0.5f;
-    
+    // Fill the array with the AudioClips to play, for example thunderclaps
+    public AudioClip[] ambientOneShots;
 
-    [Header("Random Delay Timers: ")]
-    public bool activeRandomSounds;
-    public float minDelay;
-    public float maxDelay;
+    // If you want a main overhead Ambient sound, like rain, slot here, or leave empty
+    // Dont forget setting bools
+    [Header("Main Ambient Clip: ")]
+    public AudioClip AmbientMainSound;
+    public bool isMainAmbLooping = true;
+    public float mainAmbVolume = 0.3f;
+      
+
+    // Dont forget setting bools
+    [Header("ambientOneShots] Parameters: ")]
+    public bool hasRandomInterval = true;
+    public float minInterval;
+    public float maxInterval;
+    public float ambClipVolume = 0.5f;
 
     [Header("Audio Mixer Parameters: ")]
     public bool useMixer;
@@ -31,54 +46,63 @@ public class Sound_AmbianceController : MonoBehaviour
         if(instance == null){
             instance = this;
         } 
+
+
     }
 
     void Start()
     {
-        
+        HandleMainAmbiance();
         HandleAudioSources();
 
     }
 
     void HandleAudioSources()
     {
-
+        // Makes a childObject with the new AudioSources that you use and fill them with the AudioClips
         sources = GetComponents<AudioSource>();
-        for (int i = 0; i < sources.Length; i++)
+        for (int i = 1; i < sources.Length; i++)
         {
 
             GameObject child = new GameObject("AudioSource " + i);
             child.transform.parent = gameObject.transform;
-            sources[i] = child.AddComponent<AudioSource>();
-            sources[i].clip = sounds[i];
+            sources[i] = child.AddComponent<AudioSource>(); // lägger till en ny audiousource
+            sources[i].clip = ambientOneShots[i-1];
+            //sources[0].clip = sounds[0];
             if (useMixer == true)
             {
                 sources[i].outputAudioMixerGroup = mixer;
             }
-
-            sources[0].volume = mainVol;
-            sources[0].loop = true;
-            sources[0].Play();
-                       
             
 
         }
+
+        
+    }
+
+    void HandleMainAmbiance()
+    {
+        sources[0].clip = AmbientMainSound;
+        sources[0].volume = mainAmbVolume;
+        sources[0].loop = isMainAmbLooping;
+        sources[0].Play();
     }
 
     void Update()
     {
         randomDelayedSounds();
+        sources[0].volume = mainAmbVolume;  // update volume
     }
 
     void randomDelayedSounds()
     {
         for(int i = 1; i < sources.Length; i++)
         {
-            if (!sources[i].isPlaying && activeRandomSounds)  // Fel för alla kan spela samtidigt som varandra men inte som sig själva.
+            if (!sources[i].isPlaying && hasRandomInterval)
             {
-
-                sources[i].volume = otherVol;
-                sources[Random.Range(1, sources.Length)].PlayDelayed(Random.Range(minDelay, maxDelay));
+                sources[i].volume = ambClipVolume;
+                sources[Random.Range(0, ambientOneShots.Length+1)].PlayDelayed(Random.Range(minInterval, maxInterval));
+                               
                 //sources[1].PlayDelayed(Random.Range(minDelay, maxDelay));
                 //sources[1].PlayOneShot(sounds[Random.Range(1, sounds.Length)]);
 
